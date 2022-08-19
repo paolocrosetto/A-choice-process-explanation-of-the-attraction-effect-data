333f2392ad0b17a0ea8fba9cd037ea37e95541f2
paolocrosetto <paolo.crosetto@gmail.com>
2022-08-19 14:17:17 +0200

    moved all ancillary R scripts to /Scripts and update Analysis.R accordingly

--- Analysis.R
+++ Analysis.R
@@ -41,21 +41,21 @@ df <- df %>%
          chosen = fct_recode(chosen, "Target" = "target", "Competitor" = "competitor", "Decoy" = "decoy")) 
 
 ## discretize the data to get a snapshot of the results every 10th of a second
-source("exp1_discretize.R")
+source("Scripts/exp1_discretize.R")
 
 ## Figure 4: Choice shares and difference in time, for the first click only and for all clicks, by treatment
-source("exp1_Figure_4.R")
+source("Scripts/exp1_Figure_4.R")
 
 ## Figure 5: Choice shares and difference in time, first and all clicks, by relative price
-source("exp1_Figure_5.R")
+source("Scripts/exp1_Figure_5.R")
 
 ## Figure A.3: Alternative measures of the attraction effect, by treatment
 
 ## Figure A.4: Alternative measures of the attraction effect, by relative price of the target
-source("exp1_Figure_A3_A4.R")
+source("Scripts/exp1_Figure_A3_A4.R")
 
 ## Table B.1: Dynamics of revisions: choice shares after the first click, revisions upon the second click, and choice share after two clicks
-source("exp1_Table_B1.R")
+source("Scripts/exp1_Table_B1.R")
 
 
 #### Experiment 2 ####
@@ -84,18 +84,18 @@ df <- df %>%
                               TRUE ~ 0.33))
 
 ## discretize the data to get a snapshot of the results every 10th of a second
-source("exp2_discretize.R")
+source("Scripts/exp2_discretize.R")
 
 ## Figure 8: Choice shares and difference in time, for the first click only and for all clicks, by effect
-source("exp2_Figure_8.R")
+source("Scripts/exp2_Figure_8.R")
 
 ## Figure 9: Figures 9-10-11: all effects: choice shares and difference in time, first and all clicks, by relative price
-source("exp2_Figure_9_10_11.R")
+source("Scripts/exp2_Figure_9_10_11.R")
 
 ## Table B.2: Dynamics of revisions: choice shares after the first click, revisions upon the second click, and choice share after two clicks
-source("exp2_Table_B2.R")
+source("Scripts/exp2_Table_B2.R")
 
 #### Samples of Experiments 1 and 2 ####
 
 ## Table C.1: Control and demographics variables for both experiments
-source("all_exp_Table_C1.R")
+source("Scripts/all_exp_Table_C1.R")
